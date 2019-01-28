9e64a24dff80f4d8749b2492b478e6a7ac2001c3
rkemmy <rkemmy.rk69@gmail.com>
2019-01-28 16:42:02 +0300

    Setup webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = "Remmy's Webpack React Setup";
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
